f6e12c4f497f24662b3b49a48728c28f47f754a2
Milan Bolaric <mbolaric@gmail.com>
2025-07-19 17:58:47 +0200

    VUSpeed implemented.

--- src/tachograph_gen2/vu_speed.rs
+++ src/tachograph_gen2/vu_speed.rs
@@ -2,18 +2,22 @@ use binary_data::{BinSeek, ReadBytes};
 use log::debug;
 
 use crate::Result;
-use crate::gen2::DataInfo;
-use crate::tacho::VUTransferResponseParameterID;
+use crate::gen2::{DataInfo, DataInfoGenericRecords, SignatureRecords};
+use crate::tacho::{VUTransferResponseParameterID, VuDetailedSpeedBlock};
 
 #[derive(Debug)]
-pub struct VUSpeed {}
+pub struct VUSpeed {
+    pub vu_detailed_speed_block_records: DataInfoGenericRecords<VuDetailedSpeedBlock>,
+    pub signature_records: Option<SignatureRecords>,
+}
 
 impl VUSpeed {
     pub fn from_data<R: ReadBytes + BinSeek>(trep_id: VUTransferResponseParameterID, reader: &mut R) -> Result<VUSpeed> {
         debug!("VUSpeed::from_data - Trep ID: {:?}", trep_id);
-        let detailed_speed = DataInfo::read(reader, trep_id.clone())?;
-        let signature = Some(DataInfo::read(reader, trep_id.clone())?);
+        let vu_detailed_speed_block_records: DataInfoGenericRecords<VuDetailedSpeedBlock> =
+            DataInfo::read(reader, trep_id.clone())?.parse()?;
+        let signature_records: Option<SignatureRecords> = Some(DataInfo::read(reader, trep_id.clone())?.parse()?);
 
-        Ok(Self {})
+        Ok(Self { vu_detailed_speed_block_records, signature_records })
     }
 }
