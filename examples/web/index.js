9189e12e306ad31c2f0169f2474eaf96001fe794
Milan Bolaric <milan.bolaric@ise.de>
2025-09-23 12:34:02 +0200

    Prepare Webassembly example to show data in page.

--- examples/web/index.js
+++ examples/web/index.js
@@ -1,4 +1,5 @@
 import init, { parse_from_memory } from "../../pkg/esm_parser.js";
+import { DisplayData } from "./display.js";
 
 async function run() {
   await init();
@@ -6,6 +7,7 @@ async function run() {
   const filePath = "/examples/data/Card0004.DDD";
 
   try {
+    const rootEl = document.getElementById("root-container");
     console.log(`Fetching data from ${filePath}...`);
     const response = await fetch(filePath);
 
@@ -16,14 +18,13 @@ async function run() {
     const arrayBuffer = await response.arrayBuffer();
     const data = new Uint8Array(arrayBuffer);
 
-    console.log(
-      `Successfully loaded ${data.byteLength} bytes. Calling parse_from_memory...`,
-    );
+    console.log(`Successfully loaded ${data.byteLength} bytes. Calling parse_from_memory...`);
     const start = performance.now();
     const result = await parse_from_memory(data);
+    const display = new DisplayData();
+    display.applyData(rootEl, result);
     const end = performance.now();
     console.log(`Execution time: ${end - start} ms`);
-    console.log("Success! Parsed data:", JSON.stringify(result, null, 2));
   } catch (error) {
     console.error("An error occurred:", error);
   }
