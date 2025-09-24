52b4190f09cd5ae2000637562b6b8ee465a46e7c
Milan Bolaric <milan.bolaric@ise.de>
2025-09-24 11:21:12 +0200

    Refactoring.

--- src/tachograph/vehicle_registration_number.rs
+++ src/tachograph/vehicle_registration_number.rs
@@ -4,11 +4,9 @@ use crate::{CodePage, Readable, bytes_to_string};
 
 /// Registration number of the vehicle (VRN). The registration number is
 /// assigned by the vehicle licensing authority.
-#[derive(Debug, Serialize)]
+#[derive(Debug)]
 pub struct VehicleRegistrationNumber {
-    #[serde(rename = "codePage")]
     pub code_page: CodePage,
-    #[serde(rename = "vehicleRegNumber")]
     pub vehicle_reg_number: String,
 }
 
@@ -18,5 +16,14 @@ impl Readable<VehicleRegistrationNumber> for VehicleRegistrationNumber {
         let mut vehicle_reg_number = bytes_to_string(&reader.read_into_vec(13)?, &code_page);
         vehicle_reg_number = if code_page == CodePage::Invalid { "".to_owned() } else { vehicle_reg_number };
         Ok(Self { code_page, vehicle_reg_number })
+    }
+}
+
+impl Serialize for VehicleRegistrationNumber {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.vehicle_reg_number)
     }
 }
